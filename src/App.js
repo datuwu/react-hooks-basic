60e7641dca5c4308e75529a20997ef825dad5752
datuwu <doixuihen@gmail.com>
2022-11-30 21:39:25 +0700

    Learn event

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Nav from './views/Nav';
 
 const App = () => {
   let name = "uwu"
-  let number = 2022
-  let obj = { name: "uwwU", number: 69 }
-  let link = 'https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=9'
+
+  const handleClickEvent = (event) => {
+    console.log(event, event.target);
+  }
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World and hello {name} at {number}</h1>
-        <a href={link} target='_blank'>Visit this link</a>
+        <h1>Hello World and hello {name}</h1>
+        <input type={'text'} value={'Eric'} onClick={(event) => { handleClickEvent(event) }} />
+        <button onClick={(event) => { handleClickEvent(event) }}>Visit this link</button>
       </header>
     </div>
   );
