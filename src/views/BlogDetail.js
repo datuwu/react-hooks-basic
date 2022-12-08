5ab0329649786380f404d861be10fd56899e41c8
datuwu <doixuihen@gmail.com>
2022-12-08 14:19:38 +0700

    add view blog detail feature

--- src/views/BlogDetail.js
+++ src/views/BlogDetail.js
@@ -1,6 +1,7 @@
 
 import { useParams, useHistory } from "react-router-dom"
-
+import useFetch from "../customize/fetch"
+import '../views/Blog.scss'
 
 const BlogDetail = () => {
     let { id } = useParams()
@@ -8,12 +9,29 @@ const BlogDetail = () => {
     const handleBackData = () => {
         history.push('/blog');
     }
+
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`
+    const { isLoading, isError, data: blogDetailData } = useFetch(url, false)
+
+    console.log(blogDetailData);
     return (
         <>
-            <div><span onClick={() => handleBackData()}>&lt;-- Back</span></div>
-            <h1>Blog Detail {id}</h1>
-        </>
 
+
+            <div><span onClick={() => handleBackData()}>&lt;-- Back</span></div>
+
+            {isLoading && <div style={{ textAlign: 'center !important', width: '100%' }} >Loading....</div>}
+            {!isLoading && blogDetailData &&
+                <div className="blog-detail">
+                    <div className="title">
+                        Blog ID: {id} - {blogDetailData.title}
+                    </div>
+                    <div className="content">
+                        {blogDetailData.body}
+                    </div>
+                </div>
+            }
+        </>
     )
 }
 
